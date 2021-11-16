4feb18a4b28c5f32a7c1901f08dcd90b341f516d
Antti Helkala <ahelkala@gmail.com>
2021-11-16 09:18:42 +0200

    maukas lounas ei vie saldoa neg

--- laskarit/viikko2/maksukortti/tasks.py
+++ laskarit/viikko2/maksukortti/tasks.py
@@ -1,5 +1,9 @@
 from invoke import task
 
 @task
-def test(ctx):
-    ctx.run("pytest src")
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
