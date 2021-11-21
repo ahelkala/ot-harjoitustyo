6acdda942a2d977ff45a9c59eb6b61bbb4184e11
ahelkala <ahelkala@gmail.com>
2021-11-21 17:39:26 +0200

    lisää pylint korjailuja

--- src/tests/gamegrid_test.py
+++ src/tests/gamegrid_test.py
@@ -4,12 +4,7 @@ from gamegrid import GameGrid
 class TestGameGrid(unittest.TestCase):
     def setUp(self):
         self.gamegrid = GameGrid(10,10)
-    
+
     def test_number_of_rows_and_colums_in_grid_ok(self):
-        self.assertEqual(len(self.gamegrid.grid), 10) 
-        self.assertEqual(len(self.gamegrid.grid[0]), 10) 
-
-     
-    
-
-
+        self.assertEqual(len(self.gamegrid.grid), 10)
+        self.assertEqual(len(self.gamegrid.grid[0]), 10)
