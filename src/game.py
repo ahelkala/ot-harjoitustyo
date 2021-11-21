db9b5e2c081920477b85cd7813ca233c3c50e7b6
Antti Helkala <ahelkala@gmail.com>
2021-11-21 11:40:08 +0200

    lisätty random ideoita koodiin

--- src/game.py
+++ src/game.py
@@ -1,21 +1,38 @@
 import pygame
 
-class Game:                 #first iteration of game-class. in future atleast game logic and ui will be divided to different classes
+class Game:                 
     def __init__(self):
-        pygame.init()       #initialize pygame
-        self.width, self.height = 640, 480  #for testing purposes, later these will be calculated with play area grid size
+        pygame.init()               #alustetaan pygame
+        self.block_size = 30       #peliobjektin koko pikseleinä
+        self.row_count = 9          #peliobjektien määrä rivillä
+        self.mine_count = 6
+        self.width, self.height =  self.block_size * self.row_count, self.block_size * self.row_count + 90 #varataan pieni tili tulevalle yläpalkille
         self.game_screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Miinaharava")
+        grid = GameGrid(self.row_count)     #testataan gridin luontia
+
+        
 
     def game_loop(self):
         self.game_screen.fill((0,0,0))
         pygame.display.flip()
+        
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit()
 
-seppo = Game()
 
-seppo.game_loop()
+class GameGrid:
+    def __init__(self, size):
+        self.size = size
+        self.set_grid()
+
+    def set_grid(self):
+        self.grid = [[0]*self.size for _ in range(self.size)]
+        print(self.grid)
+
+mine = Game()
+
+mine.game_loop()
