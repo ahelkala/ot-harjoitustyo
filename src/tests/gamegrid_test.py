a0889c1f2dcf5ebcd3b011203a50eb9df09b396f
ahelkala <ahelkala@gmail.com>
2021-11-30 18:12:59 +0200

    korjattu testejä

--- src/tests/gamegrid_test.py
+++ src/tests/gamegrid_test.py
@@ -12,22 +12,22 @@ class TestGameGrid(unittest.TestCase):
     
     def test_mouse_left_click_value_grows(self):
         self.gamegrid.handle_left_mouse((0,0), 30)
-        self.assertEqual(self.gamegrid[0][0], 0)
+        self.assertEqual(self.gamegrid.grid[0][0], 0)
         self.assertEqual(self.gamegrid.klicked, 0)
         self.gamegrid.handle_left_mouse((0,90), 30)
-        self.assertEqual(self.gamegrid[0][0], 20)
+        self.assertEqual(self.gamegrid.grid[0][0], 20)
         self.assertEqual(self.gamegrid.klicked, 1)
     
     def test_mouse_left_click_mine_works(self):
         self.gamegrid2 = GameGrid(10,100)
         self.gamegrid2.handle_left_mouse((0,90), 30)
         self.assertTrue(self.gamegrid2.mine_hit)
-        self.assertEqual(self.gamegrid2[0][0], 30)
+        self.assertEqual(self.gamegrid2.grid[0][0], 30)
     
     def test_mouse_right_click_flag(self):
         self.gamegrid.handle_right_mouse((0,90), 30)
-        self.assertEqual(self.gamegrid[0][0], 50)
+        self.assertEqual(self.gamegrid.grid[0][0], 50)
         self.assertEqual(self.gamegrid.flags, 1)
         self.gamegrid.handle_right_mouse((0,90), 30)
-        self.assertEqual(self.gamegrid[0][0], 0)
+        self.assertEqual(self.gamegrid.grid[0][0], 0)
         self.assertEqual(self.gamegrid.flags, 0)
