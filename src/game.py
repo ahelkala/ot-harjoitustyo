f3f903d777cf5d6f43e3a1cb302f3a7458283866
ahelkala <ahelkala@gmail.com>
2021-11-30 15:54:37 +0200

    proto melko valmis

--- src/game.py
+++ src/game.py
@@ -2,6 +2,7 @@ import pygame
 from gamegrid import GameGrid
 
 
+
 class Game:
     def __init__(self, game_area_size: int):
         # alustetaan pygame
@@ -21,29 +22,79 @@ class Game:
         # tehdään lista kuville ja täytetään se
         self.block_list = []
         self.set_block_list()
+        self.font = pygame.font.SysFont("Arial", 24)
+        self.won_time = "xxx"
 
+    #ladataan pelin käyttämät kuvat listalle
     def set_block_list(self):
         for i in range(12):
-            file_name = "src/assets/" + str(i) + ".png"
+            file_name = "assets/" + str(i) + ".png"
             self.block_list.append(pygame.transform.scale(
                 pygame.image.load(file_name), (self.block_size, self.block_size)))
 
     def loop(self):
+        self.won = False
         while True:
             self.check_events()
             self.draw()
+            self.check_if_won()
             self.clock.tick(60)
 
-    # tarkistetaan tapahtumat. ToDo hiiren oikea sekä vasen painallus.
+    #voitettiinko?
+    def check_if_won(self):
+        if self.row_count **2 - self.grid.klicked == self.mine_count and self.grid.mine_hit == False and self.won == False:
+            self.won = True
+            self.won_time = str(pygame.time.get_ticks() // 1000)
+
+    # tarkistetaan tapahtumat. 
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.handle_mouse(event.button, event.pos)
+    
+    # lähetetään hiiren sijainti oikean tai vasemman painalluksen käsittelevälle funktiolle
+    def handle_mouse(self, button, position):
+        if button == 1:
+            self.grid.handle_left_mouse(position)
+        if button > 1:
+            self.grid.handle_right_mouse(position)
 
+    #piirretään käyttöliittymä
+    #todo: tämä omaksi luokaksi?
     def draw(self):
         self.game_screen.fill((0, 0, 0))
+        self.draw_clock()
+        self.draw_mines_left()
+        self.draw_blocks()
+        pygame.display.flip()
+
+    #piirretään miinakenttä
+    def draw_blocks(self):
         for i in range(self.row_count):
             for j in range(self.row_count):
-                self.game_screen.blit(
-                    self.block_list[self.grid.grid[i][j]], (i*self.block_size, j*self.block_size + 90))
-        pygame.display.flip()
+                if 50 > self.grid.grid[i][j] > 10 :
+                    self.game_screen.blit(
+                        self.block_list[self.grid.grid[i][j]-20], (j*self.block_size, i*self.block_size + 90))
+                elif self.grid.grid[i][j] >= 50:
+                    self.game_screen.blit(
+                        self.block_list[9], (j*self.block_size, i*self.block_size + 90))
+                else:
+                    self.game_screen.blit(
+                        self.block_list[11], (j*self.block_size, i*self.block_size + 90))
+
+    #piirretään pelikello. todo: lisää tausta
+    def draw_clock(self):
+        if not self.won and self.grid.mine_hit == False:
+            play_time = str(pygame.time.get_ticks() // 1000)   
+        else:
+            play_time = self.won_time
+        display_clock = self.font.render(play_time, True, (255,0,0))
+        self.game_screen.blit(display_clock, (0,33))
+
+    #piirretään miinalaskuri. todo: lisää tausta
+    def draw_mines_left(self):
+        mines_left = str(self.mine_count - self.grid.flags)
+        display_mines_left = self.font.render(mines_left, True, (255,0,0))
+        self.game_screen.blit(display_mines_left, (self.width - 30, 33))
--- src/gamegrid.py
+++ src/gamegrid.py
@@ -1,6 +1,6 @@
 
 # Täällä luodaan peliruudukko ja alustetaan se miinoilla
-#
+# sekä hoidetaan ruudukon päivitys
 import random
 
 
@@ -10,7 +10,9 @@ class GameGrid:  # alustetaan peliruudukko
         self.mine_count = mines
         self.set_mines()
         self.set_mine_neighbours()
-        print(self.grid)
+        self.flags = 0
+        self.klicked = 0
+        self.mine_hit = False
 
     def set_mines(self):    # luodaan ruudukko ja arvotaan sille miinat
         self.grid = [[0]*self.size for _ in range(self.size)]
@@ -22,14 +24,39 @@ class GameGrid:  # alustetaan peliruudukko
                 self.grid[korkeus][leveys] = 10
                 i += 1
 
-    # Todo montako miinaa on ruutujen ympärillä
+    # lähetetään miinojen koordinaatit naapureiden laskentaan
     def set_mine_neighbours(self):
         for i in range(self.size):
             for j in range(self.size):
                 if self.grid[i][j] == 10:
                     self.set_numbers(i, j)
 
-    def set_numbers(self, i, j):  # todo saako selkeämmäksi jotenkin?
+    #hiirellä klikattu, avataan luukku ja katsotaan mitä sisällä
+    #todo: miinaan osuminen, avatun ruudun tyhjien naapureiden avaaminen
+    def handle_left_mouse(self, position):
+        position_j = position[0] // 30
+        position_i = (position[1] - 90) // 30
+        if self.grid[position_i][position_j] < 9:
+            self.grid[position_i][position_j] += 20
+            self.klicked += 1
+        if self.grid[position_i][position_j] == 10:
+            self.grid[position_i][position_j] += 20
+            self.mine_hit = True
+            
+    #oikealla hiiren klikkauksella lippu päälle/ lippu pois
+    def handle_right_mouse(self, position):
+        position_j = position[0] // 30
+        position_i = (position[1] - 90) // 30
+        if self.grid[position_i][position_j] < 11:
+            self.grid[position_i][position_j] += 50
+            self.flags += 1
+        elif self.grid[position_i][position_j] >= 50:
+            self.grid[position_i][position_j] -= 50   
+            self.flags -= 1          
+
+    #lasketaan miinan vieressä oleville ruuduille numeroarvot
+    #todo: selkiytetään koodia
+    def set_numbers(self, i, j):
         if i > 0:
             if j > 0:
                 if self.grid[i-1][j-1] != 10:
