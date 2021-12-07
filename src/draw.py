5dc33dbd560e83294999e0f7eb6aa1b4538acaad
ahelkala <ahelkala@gmail.com>
2021-12-07 19:51:04 +0200

    Käyttöliittymä siirretty omaan luokkaan + pieniä muutoksia

---
+++ src/draw.py
@@ -0,0 +1,56 @@
+import pygame
+
+class Draw:
+    def __init__(self, width, height, row_count, block_size, block_list):
+        self.font = pygame.font.SysFont("Arial", 24)
+        self.block_list = block_list
+        self.block_size = block_size
+        self.row_count = row_count
+        self.width, self.height = width, height
+        self.game_screen = pygame.display.set_mode((self.width, self.height))
+
+    # piirretään käyttöliittymä
+    def draw(self, grid, won, mine_count, won_time):
+        self.game_screen.fill((0, 0, 0))
+        self.draw_clock(won, grid, won_time)       
+        self.draw_mines_left(mine_count, grid)
+        if won:
+            self.draw_won(won_time)
+        if not won:
+            self.draw_blocks(grid)
+        pygame.display.flip()
+    
+    def draw_won(self, won_time):
+        message = "You won, your time: " + str(won_time)
+        won_string = self.font.render(message, True, (0,255,0))
+        self.game_screen.blit(won_string, (20, 120))
+
+
+    # piirretään miinalaskuri. todo: lisää tausta
+    def draw_mines_left(self, mine_count, grid):
+        mines_left = str(mine_count - grid.flags)
+        display_mines_left = self.font.render(mines_left, True, (255, 0, 0))
+        self.game_screen.blit(display_mines_left, (self.width - 30, 33))
+
+    # piirretään pelikello. todo: lisää tausta
+    def draw_clock(self, won, grid, won_time):
+        if not won and grid.mine_hit == False:
+            play_time = str(pygame.time.get_ticks() // 1000)
+        else:
+            play_time = won_time
+        display_clock = self.font.render(play_time, True, (255, 0, 0))
+        self.game_screen.blit(display_clock, (0, 33))
+
+    # piirretään miinakenttä
+    def draw_blocks(self, grid):
+        for i in range(self.row_count):
+            for j in range(self.row_count):
+                if 50 > grid.grid[i][j] > 10:
+                    self.game_screen.blit(
+                        self.block_list[grid.grid[i][j]-20], (j*self.block_size, i*self.block_size + 90))
+                elif grid.grid[i][j] >= 50:
+                    self.game_screen.blit(
+                        self.block_list[9], (j*self.block_size, i*self.block_size + 90))
+                else:
+                    self.game_screen.blit(
+                        self.block_list[11], (j*self.block_size, i*self.block_size + 90))
--- src/game.py
+++ src/game.py
@@ -1,5 +1,6 @@
 import pygame
 from gamegrid import GameGrid
+from draw import Draw
 
 
 class Game:
@@ -14,6 +15,7 @@ class Game:
         # varataan pieni tili tulevalle yläpalkille
         self.width, self.height = self.block_size * self.row_count, \
             self.block_size * self.row_count + 90
+
         self.game_screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Miinaharava")
@@ -23,6 +25,7 @@ class Game:
         self.set_block_list()
         self.font = pygame.font.SysFont("Arial", 24)
         self.won_time = "xxx"
+        self.drawer = Draw(self.width, self.height, self.row_count, self.block_size, self.block_list)
 
     # ladataan pelin käyttämät kuvat listalle
     def set_block_list(self):
@@ -35,7 +38,7 @@ class Game:
         self.won = False
         while True:
             self.check_events()
-            self.draw()
+            self.drawer.draw(self.grid, self.won, self.mine_count, self.won_time)
             self.check_if_won()
             self.clock.tick(60)
 
@@ -59,41 +62,3 @@ class Game:
             self.grid.handle_left_mouse(position, self.block_size)
         if button > 1:
             self.grid.handle_right_mouse(position, self.block_size)
-
-    # piirretään käyttöliittymä
-    # todo: tämä omaksi luokaksi?
-    def draw(self):
-        self.game_screen.fill((0, 0, 0))
-        self.draw_clock()
-        self.draw_mines_left()
-        self.draw_blocks()
-        pygame.display.flip()
-
-    # piirretään miinakenttä
-    def draw_blocks(self):
-        for i in range(self.row_count):
-            for j in range(self.row_count):
-                if 50 > self.grid.grid[i][j] > 10:
-                    self.game_screen.blit(
-                        self.block_list[self.grid.grid[i][j]-20], (j*self.block_size, i*self.block_size + 90))
-                elif self.grid.grid[i][j] >= 50:
-                    self.game_screen.blit(
-                        self.block_list[9], (j*self.block_size, i*self.block_size + 90))
-                else:
-                    self.game_screen.blit(
-                        self.block_list[11], (j*self.block_size, i*self.block_size + 90))
-
-    # piirretään pelikello. todo: lisää tausta
-    def draw_clock(self):
-        if not self.won and self.grid.mine_hit == False:
-            play_time = str(pygame.time.get_ticks() // 1000)
-        else:
-            play_time = self.won_time
-        display_clock = self.font.render(play_time, True, (255, 0, 0))
-        self.game_screen.blit(display_clock, (0, 33))
-
-    # piirretään miinalaskuri. todo: lisää tausta
-    def draw_mines_left(self):
-        mines_left = str(self.mine_count - self.grid.flags)
-        display_mines_left = self.font.render(mines_left, True, (255, 0, 0))
-        self.game_screen.blit(display_mines_left, (self.width - 30, 33))
